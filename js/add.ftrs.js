65ca709509673ce78d606e40bf6315ffd322936f
thtflx <azizsattorovthtflx@gmail.com>
2022-10-22 15:57:20 +0300

    Add new themes (js)

--- js/add.ftrs.js
+++ js/add.ftrs.js
@@ -80,43 +80,133 @@
 // 02 - Addition features (Методы массивов).
 // HTML:
 
+// JS:
+// var str = '1,2,3,4,5,6,7,8'
+
+// var array = str.split(',')
+
+// // console.log(array.join(';'))
+// // console.log(array.reverse())
+
+// array.splice(1, 0, '11', '22')
+
+// var newArray = array.concat([1, 2])
+
+// // console.log(newArray)
+
+// var objArr = [
+//     { name: 'Elon', age: 43 },
+//     { name: 'John', age: 26 },
+//     { name: 'Ugway', age: 55 }
+// ]
+
+// var findPerson = objArr.find(function(person) {
+//     return person.age === 20
+// })
+
+
+// // Чётно или Нечётно?
+// var oddArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// oddArray.filter(function(i) {
+//     return i % 2 !== 0 //выводит нечётных чисел
+//         // return i % 2 === 0 //выводит чётных чисел
+// })
+
+// // console.log(oddArray)
+
+
+// var numArray = array.map(function(i) {
+//     return parseInt(i)
+// })
+// console.log(numArray)
+
+
+
+
+// 03 - Addition features (Объект Math).
+// HTML:
 
 // JS:
-var str = '1,2,3,4,5,6,7,8'
+// var num = 2.4
 
-var array = str.split(',')
+// console.log(Math.random())
+// console.log(Math.floor(num)) //округляет в мен ьшую сторону - 2.
+// console.log(Math.ceil(num)) //округляет в большую сторону - 3.
 
-// console.log(array.join(';'))
-// console.log(array.reverse())
 
-array.splice(1, 0, '11', '22')
 
-var newArray = array.concat([1, 2])
 
-// console.log(newArray)
 
-var objArr = [
-    { name: 'Elon', age: 43 },
-    { name: 'John', age: 26 },
-    { name: 'Ugway', age: 55 }
-]
+// 04 - Addition features (Объект JSON).
+// HTML:
 
-var findPerson = objArr.find(function(person) {
-    return person.age === 20
+// JS:
+// var person = {
+//     name: 'Aziz',
+//     surname: 'Sattorov',
+//     age: 19,
+//     car: {
+//         model: 'BMW'
+//     },
+//     job: 'Frontend developer',
+//     friends: ['Azikello', 'Patrick']
+// }
+
+// var str = JSON.stringify(person) // сам код.
+
+// console.log(str) // Выводит в JSON формат
+// console.log(JSON.parse(str)) // Из JSON в OBJECT
+
+
+
+
+
+// 05 - Addition features (Дата и время с Date).
+// HTML:
+
+// JS:
+// var date = new Date()
+// (Можно посмотреть на mdn или про это расскажут еще в бонусном блоке.)
+
+
+
+
+
+// 06 - Addition features (Хранение данных в localStorage).
+// HTML:
+
+// JS:
+var button = document.querySelector('button')
+
+button.addEventListener('click', function(event) {
+    var value = document.querySelector('input').value
+
+    var obj = {
+        text: value
+    }
+
+    localStorage.setItem('headerText', JSON.stringify(obj))
+
+
+
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var obj
+
+    try {
+        obj = JSON.parse(localStorage.getItem('headerText'))
+    } catch (e) {
+        obj = {}
+    }
+
+
+    // тримом удаляем лишние пробелы и если ничего нету вернём h1 как раньше
+    if (obj && obj.text && obj.text.trim()) {
+        document.querySelector('h1').textContent = obj.text
+    }
 })
 
 
-// Чётно или Нечётно?
-var oddArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-oddArray.filter(function(i) {
-    return i % 2 !== 0 //выводит нечётных чисел
-        // return i % 2 === 0 //выводит чётных чисел
-})
-
-// console.log(oddArray)
-
-
-var numArray = array.map(function(i) {
-    return parseInt(i)
-})
-console.log(numArray)
+// Повторить вот эти пройденные темы (прочитать документацию.)
